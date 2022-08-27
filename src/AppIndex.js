a557d080239dc78b489ade0ba37b5a6120c64416
Sunday <misterdavid2100@gmail.com>
2022-08-27 23:15:56 +0100

    form field setup

--- src/AppIndex.js
+++ src/AppIndex.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MainNav from './components/MainNav';
-import Button from './components/elements/Button';
+// import Button from './components/elements/Button';
+import FieldBlock from './components/elements/FieldBlock';
 
 function AppIndex(){
+    const [username, setUsername] = useState();
+    const [password, SetPassword] = useState();
     return (
         <main className="app"> 
             <MainNav />
             <div className="main-content">
-                <Button variant="primary" onClick={()=> window.alert('Button Clicked')}>
+                <h2> Your Username will be: {username}</h2>
+                <FieldBlock id="username" value={username}
+                    onChange={(evt) => {setUsername(evt.target.value)}} 
+                    label="Username:"
+                />
+                <FieldBlock id="password" value={password} 
+                    onChange={(evt)=>{SetPassword(evt.target.value)}}
+                    label="Password:" type="password" feedback="must be 8 characters" isInvalid={true}
+                />
+                    
+                {/* <Button variant="primary" onClick={()=> window.alert('Button Clicked')}>
                     Btn One <span>&times;</span> 
-                </Button>
-                <Button variant="danger">
-                    Btn Two 
-                </Button>
-                <Button>
-                    Button Three
-                </Button>
-                {/* <Button> Button Two</Button> */}
-                {/* <Button label="Btn Two" /> */}
+                </Button> */}
+                
             </div>
             
         </main>
