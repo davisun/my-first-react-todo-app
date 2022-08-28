12ee61846c66d442c00028faa8a50d69b83f612b
Sunday <misterdavid2100@gmail.com>
2022-08-28 16:07:29 +0100

    added ajax helper and made a connection to node server

--- src/AppIndex.js
+++ src/AppIndex.js
@@ -2,15 +2,17 @@ import React, {useState} from 'react';
 import MainNav from './components/MainNav';
 // import Button from './components/elements/Button';
 import FieldBlock from './components/elements/FieldBlock';
+import { jsonPost, jsonGet } from './helpers/Ajax';
 
 function AppIndex(){
     const [username, setUsername] = useState();
     const [password, SetPassword] = useState();
+    jsonGet('contacts/1',  (resp) => {console.log(resp)});
     return (
         <main className="app"> 
             <MainNav />
             <div className="main-content">
-                <h2> Your Username will be: {username}</h2>
+                <h2> Your API Domain is: {process.env.REACT_APP_API}</h2>
                 <FieldBlock id="username" value={username}
                     onChange={(evt) => {setUsername(evt.target.value)}} 
                     label="Username:"
